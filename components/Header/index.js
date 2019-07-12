8689f956bea5979ca5f5154d9fd737f7df4c9217
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-07-12 10:26:12 -0500

    WIP: index.js complete.

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,35 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+
+    // define new elements 
+    const header = document.createElement("div"); 
+    const date = document.createElement("span"); 
+    const headerh1 = document.createElement("h1"); 
+    const temp = document.createElement("span"); 
+
+    // assign class names 
+    header.classList.add("header"); 
+    date.classList.add("date"); 
+    temp.classList.add("temp"); 
+
+    // set text content 
+    date.textContent = "March 28, 2019"; 
+    headerh1.textContent = "Lambda Times"; 
+    temp.textContent = "98°"; 
+
+    // create structure 
+    header.appendChild(date);
+    header.appendChild(headerh1);
+    header.appendChild(temp);
+
+    return header 
+}
+
+// get headerContainer div 
+const headerContainer = document.querySelector(".header-container"); 
+console.log(headerContainer); 
+
+// append header to headerContainer div 
+headerContainer.appendChild(Header()); 
