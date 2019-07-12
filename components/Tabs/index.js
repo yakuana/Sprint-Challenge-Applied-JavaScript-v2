a440b4ee6d0c2990879cc020b231dabd7732ef14
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-07-12 10:50:44 -0500

    WIP: Header and Tabs index.js complete.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector(".topics")
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then ((data) => {
+        //successful 
+        console.log("returned data and data type:", data.constructor.name, data); 
+
+        const topicsArray = data.data.topics; 
+        console.log("topics array:", data.data.topics.constructor.name, topicsArray); 
+
+        topicsArray.forEach((element) => {
+            // define element  
+            const tab = document.createElement("div"); 
+
+            // add tab class 
+            tab.classList.add("tab"); 
+
+            // add text content using "element" arguement 
+            tab.textContent = element; 
+
+            // append element to topics div 
+            topics.appendChild(tab); 
+        })
+
+    })
+
+    .catch ((error) => {
+        // unsuccessful 
+        console.log("The API is currently down.", error)
+    });
